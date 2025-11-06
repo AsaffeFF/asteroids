a18594b7f2e2263b74a9c2c657415e3251ed8dbf
AsaffeFF <asaffeferraz002@gmail.com>
2025-11-06 11:33:56 -0300

    finalizado

--- asteroid.py
+++ asteroid.py
@@ -16,10 +16,19 @@ class Asteroid(CircleShape):
         self.position += self.velocity * dt
 
     def split(self):
-        self.kill()
+        
         if self.radius <= ASTEROID_MIN_RADIUS:
+            self.kill()
             return
         log_event("asteroid_split")
-        angle = random.uniform(20, 50)
-        
-    
+        random_angle = random.uniform(20, 50)
+        x1 = self.velocity.rotate(random_angle)
+        x2 = self.velocity.rotate(-random_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        asteroid1.velocity = x1 * 1.2
+        asteroid2.velocity = x2 * 1.2
+        self.kill()
